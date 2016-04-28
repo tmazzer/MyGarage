109a345eb46ca72d6be9ed64709e830f81372a15
Juliano Zapelini <Juliano Zapelini>
2016-04-28 18:10:57 -0300

    Login funcionando. Timeline draft

--- src/opet/mygarage/controller/AmigosBean.java
+++ src/opet/mygarage/controller/AmigosBean.java
@@ -3,55 +3,48 @@
  */
 package opet.mygarage.controller;
 
-import java.io.Serializable;
-
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
-@Named("timelineBean")
-@SessionScoped
+@Named("amigosBean")
+@RequestScoped
 /**
- * Backing Bean para dar suporte às páginas Timeline/JSF
+ * Backing Bean para dar suporte às páginas Amigos/JSF
  * 
  * @author Juliano Zapelini Batista / Tiago Mazzer
  * 
- * @since 26/04/2016
+ * @since 28/04/2016
  * 
  * @version 1.0
  * 
  */
-public class TimelineBean implements Serializable {
-
+public class AmigosBean {
 	/*
 	 * Variáveis de instância
 	 */
 	
 	private String msgRetorno;
 	
-	private static final long serialVersionUID = -911596375118728823L;
-
 	/*
 	 * Função construtora da classe
 	 */
 	/**
 	 * Construtor TimelineBean()
 	 */
-	public TimelineBean() {
-		System.out.println("TimelineBean::Construtor()");
-		msgRetorno = "DEU BOOOOOA";
+	public AmigosBean() {
+		System.out.println("AmigosBean::Controller");
 	}
-	
 	/*
 	 * Métodos de acesso
 	 */
 	/**
-	 * Monta a tela inicial timelineView.xhtml
+	 * Monta a tela inicial amigosView.xhtml
 	 */
 	public void inicializaPagina()
 	{
-		System.out.println("TimelineBean::inicializaPagina()");
+		System.out.println("AmigoBean::inicializaPagina()");
+		msgRetorno = "Entrou na AmigosView";
 	}
-
 	/**
 	 * @return the msgRetorno
 	 */
@@ -65,6 +58,5 @@ public class TimelineBean implements Serializable {
 	public void setMsgRetorno(String msgRetorno) {
 		this.msgRetorno = msgRetorno;
 	}
-
 
 }
